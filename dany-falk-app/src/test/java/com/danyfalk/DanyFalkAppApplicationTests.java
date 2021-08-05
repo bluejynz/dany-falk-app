7d7f56ee796ecada60b645fcb741d42b19a56413
bluejynz <danifalk27@gmail.com>
2021-08-05 15:34:40 -0300

    Initial commit

---
+++ dany-falk-app/src/test/java/com/danyfalk/DanyFalkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danyfalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanyFalkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
